e6d6d73178fa5586d476c3b9a3cb6f41ad2cc0d7
VP <reservvp@gmail.com>
2025-08-11 12:33:17 +0300

    prepare project

---
+++ Best Recipes/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  Best Recipes
+//
+//  Created by VP on 11.08.2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowsScene = (scene as? UIWindowScene) else { return }
+        let vc = ViewController()
+        let window = UIWindow(windowScene: windowsScene)
+        window.rootViewController = vc
+        self.window = window
+        
+        window.makeKeyAndVisible()
+    }
+}
