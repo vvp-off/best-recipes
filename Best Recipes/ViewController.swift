2a253fded2f48ed42d294c276a08170ce4c3e163
vp <vp>
2025-08-11 12:04:02 +0300

    Initial Commit

---
+++ Best Recipes/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Best Recipes
+//
+//  Created by VP on 11.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
