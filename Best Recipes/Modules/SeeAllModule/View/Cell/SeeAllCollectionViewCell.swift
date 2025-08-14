7335128a40d4f2bcaa8df8c5215ec3a042891f1e
Ivan <izzuyinvest@gmail.com>
2025-08-14 18:40:52 +0300

    feat(seeAll): add SeeAllViewController

---
+++ Best Recipes/Modules/SeeAllModule/View/Cell/SeeAllCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SeeAllCollectionViewCell.swift
+//  Best Recipes
+//
+//  Created by Иван Семенов on 14.08.2025.
+//
+
+import UIKit
+
+class SeeAllCollectionViewCell: UICollectionViewCell {
+    
+}
