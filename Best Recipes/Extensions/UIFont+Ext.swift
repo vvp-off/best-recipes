4a776ce02877196916b52f9506955e71ab253ab8
Ivan <izzuyinvest@gmail.com>
2025-08-15 18:34:15 +0300

    feat(onboarding): add StartViewController

---
+++ Best Recipes/Extensions/UIFont+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  UIFont+Ext.swift
+//  Best Recipes
+//
+//  Created by Иван Семенов on 15.08.2025.
+//
+
+import Foundation
---
+++ Best Recipes/Modules/OnboardingModule/StartViewController.swift
@@ -0,0 +1,8 @@
+//
+//  StartViewController.swift
+//  Best Recipes
+//
+//  Created by Иван Семенов on 15.08.2025.
+//
+
+import Foundation
