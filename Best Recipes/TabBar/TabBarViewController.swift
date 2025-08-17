667bab5179d348f8f0ac633b500695c15c4b07aa
VP <reservvp@gmail.com>
2025-08-17 16:51:46 +0300

    •    chore: cleanup project structure

--- Best Recipes/TabBar/TabBarViewController.swift
+++ Best Recipes/TabBar/TabBarViewController.swift
@@ -52,15 +52,15 @@ final class TabBarViewController: UITabBarController {
     
 //  MARK: - TabBarItem and ViewControllers setup
     private func getMockVC() -> [UIViewController] {
-        let firstVC = ViewController()
+        let firstVC = UIViewController()
         firstVC.view.backgroundColor = .black
         let secondVC = FavoriteViewController.create() //Stub
         secondVC.view.backgroundColor = .white
-        let thirdVC = ViewController()
+        let thirdVC = UIViewController()
         thirdVC.view.backgroundColor = .green
-        let forVC = ViewController()
+        let forVC = UIViewController()
         forVC.view.backgroundColor = .darkGray
-        let fiveVC = ViewController()
+        let fiveVC = UIViewController()
         fiveVC.view.backgroundColor = .brown
         let mockViewControllers = [firstVC, secondVC, thirdVC, forVC, fiveVC]
         return mockViewControllers
